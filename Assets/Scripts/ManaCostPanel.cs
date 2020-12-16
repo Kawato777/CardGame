d69dc515be50c8249e6a0f06a708df916d5b2e2d
Kawato26 <keiaru0512@gmail.com>
2020-12-16 20:22:15 +0900

    Card&Field作成完了

---
+++ Assets/Scripts/ManaCostPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManaCostPanel : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI manaCostText;
+
+    public int defaultManaCost;
+    public int manaCost;
+
+    void Start()
+    {
+        SetManaCostText(manaCost, defaultManaCost);
+    }
+
+    public void SetManaCostText(int manaCost,int defaultManaCost)
+    {
+        manaCostText.text = $"{manaCost} / {defaultManaCost}";
+    }
+}
