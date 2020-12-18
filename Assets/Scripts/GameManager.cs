1202769a202a1e54176798793c0c5116ae830064
Kawato26 <keiaru0512@gmail.com>
2020-12-18 20:18:49 +0900

    カードドラッグ完了

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    [SerializeField]
+    GameObject cardPrefab;
+    [SerializeField]
+    Transform playerHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cardPrefab, playerHand);    // カード生成
+    }
+
+}
